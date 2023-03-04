a67295654088a594cf700cbb1a1d60a4e689cb82
Dorkious <65192764+duderacer24@users.noreply.github.com>
2023-03-04 08:37:12 -0600

    Minor fix to make console in Main not null

--- src/main/java/com/github/lorddorkious/polyshop/Main.java
+++ src/main/java/com/github/lorddorkious/polyshop/Main.java
@@ -37,6 +37,7 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        console = this.getServer().getConsoleSender();
         console.sendMessage(POLYSHOP_PREFIX + " Is Enabled");
     }
 
